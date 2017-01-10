2a18e8ebcc6f3af6a567fb1cf83cac9e4adad7e3
Nate Rabins <nrabins@alkamitech.com>
2017-01-10 15:46:18 -0600

    Initial commit

---
+++ app/src/main/java/com/example/nrabins/versioncontroldemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.nrabins.versioncontroldemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
